ae35ad338fbf0b7c33e65b09868e78d20cc80dcc
szwedas <164246991+szwedas@users.noreply.github.com>
2025-07-04 03:03:58 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,18 +1,6 @@
 <?php
 session_start();
 
-$users = array(
-    "szwedas" => "masturbacja",
-    // Info o autorze itd.
-); 
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $username = $_POST['szwedas'];
-    $password = $_POST['masturbacja'];
-
-    if (array_key_exists($username, $users) && $users[$username] === $password) {
-        $_SESSION['loggedin'] = true;
-        $_SESSION['username'] = $username;
         header("Location: dashboard.php");
         } else {
         header("Location: dashboard.php");
